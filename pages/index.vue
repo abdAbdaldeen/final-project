29ea821c150ed4a59b3266be4ddb3ac41d55ae0c
abdaldeen <abdelrahman.abdaldeen@mawdoo3.com>
2022-02-19 15:59:07 +0200

    add vuetify

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,31 @@
 <template>
-  <Tutorial />
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-text-field
+            :counter="10"
+            label="الاسم الاول"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="12" md="4">
+          <v-text-field
+            :counter="10"
+            label="اسم الاخير"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="12" md="4">
+          <v-text-field label="البريد الالكتروني" required></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-btn @click="$vuetify.theme.dark = true">{{ $vuetify.theme.dark }}</v-btn>
+  </v-form>
 </template>
 
 <script>
@@ -7,3 +33,5 @@ export default {
   name: 'IndexPage',
 }
 </script>
+
+<style lang="scss"></style>
