e7bb33901e59d0edfd3a69c38c85e59dd82d2173
Malak-Mustafa <malak2000moayad@gmail.com>
2022-02-23 21:12:25 +0200

    create form page and modify style file

---
+++ pages/FormPage.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-form>
+    <v-container>
+      <v-row id="sign-in">
+        <h1>Sign in form</h1>
+        <v-col cols="12" md="4">
+          <v-text-field
+            :counter="10"
+            label="الاسم الاول"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="12" md="4">
+          <v-text-field
+            :counter="10"
+            label="اسم الاخير"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="12" md="4">
+          <v-text-field label="البريد الالكتروني" required></v-text-field>
+        </v-col>
+        <v-btn @click="$vuetify.theme.dark = true">{{
+          $vuetify.theme.dark
+        }}</v-btn>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: 'FormPage',
+}
+</script>
+
+<style  lang="scss" scoped>
+$fontSize: 30px;
+$BGColor: #eeeeee;
+h1 {
+  font-size: $fontSize;
+}
+#sign-in {
+  background-color: $BGColor;
+}
+</style>
