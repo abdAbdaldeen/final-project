356c8e5b64753bda6c9c9b0d59cd85f953e60cf5
Malak-Mustafa <malak2000moayad@gmail.com>
2022-03-05 21:00:40 +0300

    add Categories Bar to the header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,46 +1,51 @@
 <template>
-  <header class="header">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <v-container class="container">
-      <h1 class="logo">مبرمجون</h1>
-      <form class="search-form">
-        <input type="text" placeholder="ابحث عن سؤال" class="search" />
-        <v-icon class="search-icon" color="primary">search</v-icon>
-      </form>
+  <div>
+    <header class="header">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <v-container class="container">
+        <h1 class="logo">مبرمجون</h1>
+        <form class="search-form">
+          <input type="text" placeholder="ابحث عن سؤال" class="search" />
+          <v-icon class="search-icon" color="primary">search</v-icon>
+        </form>
 
-      <v-icon
-        v-if="$vuetify.theme.dark == true"
-        @click="$vuetify.theme.dark = !$vuetify.theme.dark"
-      >
-        light_mode
-      </v-icon>
-      <v-icon
-        v-else
-        class="mode-icon"
-        @click="$vuetify.theme.dark = !$vuetify.theme.dark"
-        >dark_mode</v-icon
-      >
-
-      <v-btn
-        v-if="false"
-        href="/FormPage"
-        color="primary"
-        class="btn"
-        :small="$vuetify.breakpoint.xsOnly"
-        >تسجيل الدخول</v-btn
-      >
-      <v-avatar
-        v-else
-        class="avatar"
-        color="primary"
-        :size="$vuetify.breakpoint.xsOnly ? '30' : '43'"
-      >
-        <v-icon :small="$vuetify.breakpoint.xsOnly" color="background"
-          >account_circle</v-icon
+        <v-icon
+          v-if="$vuetify.theme.dark == true"
+          @click="$vuetify.theme.dark = !$vuetify.theme.dark"
         >
-      </v-avatar>
-    </v-container>
-  </header>
+          light_mode
+        </v-icon>
+        <v-icon
+          v-else
+          class="mode-icon"
+          @click="$vuetify.theme.dark = !$vuetify.theme.dark"
+          >dark_mode</v-icon
+        >
+
+        <v-btn
+          v-if="false"
+          href="/FormPage"
+          color="primary"
+          class="btn"
+          :small="$vuetify.breakpoint.xsOnly"
+          >تسجيل الدخول</v-btn
+        >
+        <v-avatar
+          v-else
+          class="avatar"
+          color="primary"
+          :size="$vuetify.breakpoint.xsOnly ? '30' : '43'"
+        >
+          <v-icon :small="$vuetify.breakpoint.xsOnly" color="background"
+            >account_circle</v-icon
+          >
+        </v-avatar>
+      </v-container>
+    </header>
+    <nav>
+      <CategoriesBar/>
+    </nav>
+  </div>
 </template>
 
 <script>
@@ -72,8 +77,8 @@ header {
     border-radius: 10px;
   }
 
-  .search-form:focus {
-    border: 2px solid #4ecca3;
+  .search-form:hover {
+    border: 2px solid var(--v-primary-base);
   }
 
   .search-icon {
