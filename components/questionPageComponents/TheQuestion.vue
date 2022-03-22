5b830b4ed80ba3c0b5717f05d9c217872ec0fc17
Malak-Mustafa <malak2000moayad@gmail.com>
2022-03-22 20:25:01 +0300

    create Question page

---
+++ components/questionPageComponents/TheQuestion.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{ data.title }}</h1>
+    <hr />
+    <p>{{ data.body }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheQuestion',
+  props: {
+    data: {
+      type: Object,
+      default: () => {
+        return {
+          title: 'ما هو الفرق بين Methods و Computed في Vue',
+          votesNum: 0,
+          body: 'ما هو الفرق الرئيسي بين methods and a computed value في Vue.js? انها تبدو متشابهة وقابلة للتبديل',
+          category: 'vue',
+        }
+      },
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ pages/q/_id.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <v-container>
+      <the-question />
+    </v-container>
+  </section>
+</template>
+
+<script>
+import TheQuestion from '~/components/questionPageComponents/TheQuestion.vue'
+export default {
+  components: { TheQuestion },
+}
+</script>
+
+<style></style>
