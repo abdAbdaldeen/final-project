c9c6e6296fb375726471e3d9084c31e6e1f78db9
Malak-Mustafa <malak2000moayad@gmail.com>
2022-03-22 22:29:38 +0300

    create component

--- components/questionPageComponents/AddVote.vue
+++ components/questionPageComponents/AddVote.vue
@@ -1,9 +1,32 @@
 <template>
-  <div></div>
+  <div class="addVoteComponent">
+    <v-icon class="voteIcon" large @click="votesNum++">arrow_drop_up</v-icon>
+    <p class="QPvotes-num">{{ votesNum }}</p>
+    <v-icon class="voteIcon" large @click="votesNum--">arrow_drop_down</v-icon>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'AddVote',
+  data() {
+    return {
+      votesNum: 100,
+    }
+  },
+}
 </script>
 
-<style></style>
+<style lang="scss">
+.addVoteComponent {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  opacity: 0.7;
+  .voteIcon {
+  }
+  .QPvotes-num {
+    margin: 0;
+  }
+}
+</style>
