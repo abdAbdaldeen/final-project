418370141fe56eb7dc03bda46b266794ca23f482
Malak-Mustafa <malak2000moayad@gmail.com>
2022-03-23 18:24:01 +0300

    add components

--- pages/q/_id.vue
+++ pages/q/_id.vue
@@ -1,16 +1,35 @@
 <template>
   <section>
-    <v-container>
+    <v-container class="TheQuestionPageContainer">
       <the-question />
+      <br />
+      <div class="AnswersContainer">
+        <h1>الإجابات</h1>
+        <answer-box />
+      </div>
+      <v-form class="AddAnswerForm">
+        <v-textarea outlined name="input-7-4" label="اضف اجابتك"></v-textarea>
+        <v-btn class="AddAnswerBtn" type="submit" color="primary"> نشر </v-btn>
+      </v-form>
     </v-container>
   </section>
 </template>
 
 <script>
 import TheQuestion from '~/components/questionPageComponents/TheQuestion.vue'
+import AnswerBox from '~/components/questionPageComponents/AnswerBox.vue'
 export default {
-  components: { TheQuestion },
+  components: { TheQuestion, AnswerBox },
 }
 </script>
 
-<style></style>
+<style lang="scss">
+.TheQuestionPageContainer {
+  .AddAnswerForm {
+    text-align: center;
+  }
+  .AddAnswerBtn {
+    width: 7rem;
+  }
+}
+</style>
