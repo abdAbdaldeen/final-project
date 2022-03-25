71b96c99c7e05e24c6e750f009a7a91670f908d6
abdaldeen <abdelrahman.abdaldeen@mawdoo3.com>
2022-03-25 20:45:15 +0300

    question page

--- pages/q/_id.vue
+++ pages/q/_id.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <v-container class="TheQuestionPageContainer">
-      <the-question />
+      <the-question :data="data"/>
       <br />
       <div class="AnswersContainer">
         <h1>الإجابات</h1>
@@ -20,6 +20,17 @@ import TheQuestion from '~/components/questionPageComponents/TheQuestion.vue'
 import AnswerBox from '~/components/questionPageComponents/AnswerBox.vue'
 export default {
   components: { TheQuestion, AnswerBox },
+  async asyncData({ $axios, store, error, redirect, route }) {
+    return await $axios
+      .$get('questions/getOne/'+route.params.id)
+      .then((r) => {
+        return {data:r};
+      })
+      .catch((e) => {
+        console.error(e)
+        // error({ statusCode: 404, message: 'Post not found' });
+      })
+  },
 }
 </script>
 
