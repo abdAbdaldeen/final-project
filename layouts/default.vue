9eeea88ae4f1bd6db7e696c9d52f750ed06315da
abdaldeen <abdelrahman.abdaldeen@mawdoo3.com>
2022-03-30 22:29:22 +0300

    on load, login check, and get user data

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,41 @@
 <template>
   <v-app>
-    <AppHeader/>
+    <AppHeader />
     <Nuxt />
   </v-app>
 </template>
 <script>
 export default {
   async fetch() {
-    await this.$axios.$get("groups/get")
-    .then((res)=>{
-      this.$store.commit("categories/add", res);
+    await this.$axios.$get('groups/get').then((res) => {
+      this.$store.commit('categories/add', res)
     })
   },
-  created(){
-    // if (process.client) {
-    //   let authToken = localStorage.getItem('authToken')
-    //   if (authToken) {
-    //     this.$store.commit("categories/add", res)
-    //   }
-    // }
-  }
-};
+  async created() {
+    if (process.client) {
+      const token = localStorage.getItem('authToken')
+      if (token) {
+        await this.$axios
+          .$get('users/get', {
+            headers: {
+              authorization: `Bearer ${token}`,
+            },
+          })
+          .then((res) => {
+            const data = {
+              token,
+              email: res.email,
+              displayName: res.displayName,
+              photoURL: res.photoURL,
+            }
+            this.$store.commit('user/login', data)
+          })
+          .catch((e) => {
+            console.error(e)
+          })
+      }
+    }
+  },
+}
 </script>
-<style>
-</style>
+<style></style>
