7a8d6ba76269d2da7b822b1482aee9cf51730520
Malak-Mustafa <malak2000moayad@gmail.com>
2022-04-07 17:09:37 +0300

    add validation rules

--- pages/q/_id.vue
+++ pages/q/_id.vue
@@ -1,14 +1,25 @@
 <template>
   <section>
     <v-container class="TheQuestionPageContainer">
-      <the-question/>
+      <the-question />
       <br />
-      <div class="AnswersContainer" v-if="data.answers">
+      <div v-if="data.answers" class="AnswersContainer">
         <h1>الإجابات</h1>
-        <answer-box v-for="answer in data.answers" :key="answer.aID" :data="answer"/>
+        <answer-box
+          v-for="answer in data.answers"
+          :key="answer.aID"
+          :data="answer"
+        />
       </div>
-      <v-form class="AddAnswerForm" @submit.prevent="submit">
-        <v-textarea outlined name="input-7-4" label="اضف اجابتك" v-model="answer"></v-textarea>
+      <v-form ref="form" class="AddAnswerForm" @submit.prevent="submit">
+        <v-textarea
+          v-model="answer"
+          outlined
+          name="input-7-4"
+          label="اضف اجابتك"
+          :rules="answerRules"
+          required
+        ></v-textarea>
         <v-btn class="AddAnswerBtn" type="submit" color="primary"> نشر </v-btn>
       </v-form>
     </v-container>
@@ -22,17 +33,21 @@ export default {
   components: { TheQuestion, AnswerBox },
   async asyncData({ $axios, store, error, redirect, route }) {
     return await $axios
-      .$get('questions/getOne/'+encodeURIComponent(route.params.id))
+      .$get('questions/getOne/' + encodeURIComponent(route.params.id))
       .then((r) => {
-        return store.commit("question/add", r);
+        return store.commit('question/add', r)
       })
       .catch((e) => {
+        // eslint-disable-next-line no-console
         console.error(e)
         // error({ statusCode: 404, message: 'Post not found' });
       })
   },
+
   data: () => ({
-    answer: "",
+    answer: '',
+    answerRules: [(v) => !!v || 'الاجابة مطلوبة'],
+
     loading: false,
   }),
   computed: {
@@ -42,25 +57,32 @@ export default {
   },
   methods: {
     async submit() {
-      try {
-        this.loading = true
-        const res = await this.$axios.post('answers/add', {
-          body: this.answer,
-          qID: this.$route.params.id,
-        },{
-            headers: {
-              authorization: `Bearer ${localStorage.getItem('authToken')}`,
+      if (this.$refs.form.validate()) {
+        try {
+          this.loading = true
+          const res = await this.$axios.post(
+            'answers/add',
+            {
+              body: this.answer,
+              qID: this.$route.params.id,
             },
-            params: {
-              getUserData: 1,
+            {
+              headers: {
+                authorization: `Bearer ${localStorage.getItem('authToken')}`,
+              },
+              params: {
+                getUserData: 1,
+              },
             }
-          })
-        console.log(res)
-        // this.loading = false;
-      } catch (error) {
-        // eslint-disable-next-line no-console
-        console.error(error)
-        this.loading = false
+          )
+          // eslint-disable-next-line no-console
+          console.log(res)
+          // this.loading = false;
+        } catch (error) {
+          // eslint-disable-next-line no-console
+          console.error(error)
+          this.loading = false
+        }
       }
     },
   },
