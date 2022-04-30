372897ffc724843075f5cef0f85e94508e8c7520
Malak-Mustafa <malak2000moayad@gmail.com>
2022-05-01 01:55:47 +0300

    edit style

--- components/QuestionBox.vue
+++ components/QuestionBox.vue
@@ -2,11 +2,15 @@
   <v-container class="QBoxContainer">
     <nuxt-link :to="'/q/' + data.qID">
       <div class="q-box">
-        <h1 class="QBtitle">{{ data.title }}</h1>
-        <span class="QBvotes-num">{{ data.votesCount }} صوت</span>
+        <div class="row1">
+          <h1 class="QBtitle">{{ data.title }}</h1>
+          <p class="QBvotes-num">{{ data.votesCount + '   صوت' }}</p>
+        </div>
         <p class="QBbody">{{ data.body.slice(0, 150) + '...' }}</p>
-        <span class="QBcategory">{{ data.groupID }}</span>
-        <p class="QBdate">تم النشر في {{ data.createdAt }}</p>
+        <div class="row3">
+          <p class="QBcategory">{{ data.groupID }}</p>
+          <p class="QBdate">تم النشر في {{ data.createdAt }}</p>
+        </div>
       </div></nuxt-link
     >
   </v-container>
@@ -36,61 +40,54 @@ export default {
   a {
     text-decoration: none;
   }
+
   .q-box {
     //background-color: var(--v-qBoxBg-base);
     box-shadow: $boxShadow;
-    display: grid;
-    align-items: start;
+
     padding: 1rem 2rem;
     border-radius: 10px;
     color: var(--v-textColor-base);
-
-    .QBtitle {
-      padding-bottom: 1rem;
-      font-weight: bold;
-      max-width: 60rem;
-    }
-    .QBvotes-num {
-      box-shadow: 0px 0px 5px var(--v-secondary-lighten5);
-      border-radius: 4px;
-
-      // border: 2px solid var(--v-primary-base);
-      grid-row: 1;
-      grid-column: 6;
-      // max-width: 2rem;
-      justify-self: flex-end;
-      padding: 0.4rem 0.8rem;
-
-      text-align: center;
-      grid-template-rows: end;
+    .row1 {
+      display: flex;
+      justify-content: space-between;
+      .QBtitle {
+        padding-bottom: 1rem;
+        font-weight: bold;
+        max-width: 60rem;
+      }
+      .QBvotes-num {
+        box-shadow: 0px 0px 5px var(--v-secondary-lighten5);
+        border-radius: 4px;
+        padding: 0.5rem 0.8rem;
+        align-self: center;
+      }
     }
     .QBbody {
-      grid-row: 2;
     }
-    .QBcategory {
-      grid-row: 3;
-      // border: 2px solid var(--v-primary-base);
-      box-shadow: 0px 0px 5px var(--v-secondary-lighten5);
-      border-radius: 4px;
-      // max-width: 5rem;
-      justify-self: flex-start;
-      padding: 0.5rem 1rem;
-      text-align: center;
-    }
-    .QBdate {
-      grid-row: 3;
-      grid-column: 6;
-      justify-self: flex-end;
+    .row3 {
+      display: flex;
+      justify-content: space-between;
+
+      .QBcategory {
+        // border: 2px solid var(--v-primary-base);
+        box-shadow: 0px 0px 5px var(--v-secondary-lighten5);
+        border-radius: 4px;
+        // max-width: 5rem;
+        padding: 0.5rem 1rem;
+        text-align: center;
+      }
     }
     @media (max-width: 768px) {
       padding: 0.5rem 1rem;
 
       .QBtitle {
         font-size: 1rem !important;
-        padding-bottom: 0.5rem;
+
         padding-left: 1rem;
       }
       .QBvotes-num {
+        text-align: center;
         min-width: 1.5rem;
         font-size: 0.7rem;
       }
@@ -100,7 +97,9 @@ export default {
       .QBcategory {
         font-size: 0.7rem;
         padding: 0.5rem 0.5rem;
-        width: 3rem;
+      }
+      .QBdate {
+        font-size: 0.7rem !important;
       }
     }
   }
